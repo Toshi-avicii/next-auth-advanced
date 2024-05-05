52496a901c15dc4b84f53b2c42abf84cf10cc882
Tushar Arora <tushar.toshi12@gmail.com>
2024-05-05 15:30:53 +0530

    adding role based authentication

--- auth.ts
+++ auth.ts
@@ -2,8 +2,28 @@ import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from "@/lib/db";
+import { getUserById } from "@/data/user";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if(token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      console.log(session);
+      return session;
+    },
+    async jwt({ token }) {
+      if(!token.sub) {
+        return token;
+      }
+      const existingUser = await getUserById(token.sub);
+      if(!existingUser) {
+        return token;
+      }
+      return token;
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig
